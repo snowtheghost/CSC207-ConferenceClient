3a36917282be06063cfdc8cc201cdfbe2116a3c4
Zachary Vincze <zacharyvincze@gmail.com>
2020-10-29 19:00:09 -0400

    Started with abstract User class definition.

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,2 +1,19 @@
+import java.util.UUID;
+
+/**
+ * Represents an abstract User
+ * @author Zachariah Vincze
+ */
 public abstract class User {
+    private UUID userID;
+    private String username;
+
+    /**
+     * Creates a new user with a unique ID and a username.
+     * @param username The user's username.
+     */
+    public User(String username) {
+        this.userID = UUID.randomUUID();
+        this.username = username;
+    }
 }
