a961fecdd8883ad416e191c32b5a465e35734eec
Kaiyi <kaiyiliu21@gmail.com>
2020-11-01 19:41:21 -0500

    Created Constructor for Organizer Class

--- phase1/src/Organizer.java
+++ phase1/src/Organizer.java
@@ -1,2 +1,11 @@
 public class Organizer extends User {
+
+    /**
+     * Creates a new user with a unique ID and a username.
+     *
+     * @param username The user's username.
+     */
+    public Organizer(String username) {
+        super(username);
+    }
 }
