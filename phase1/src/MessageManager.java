64184702a36abfdeff240d76a5f8c9a24f719ac8
Zachary Vincze <zacharyvincze@gmail.com>
2020-11-03 21:57:37 -0500

    Started MessageManager implementation

--- phase1/src/MessageManager.java
+++ phase1/src/MessageManager.java
@@ -1,2 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Manages message entities.
+ * @author Zachariah Vincze
+ */
 public class MessageManager {
+    private Map<UUID, Message> messages;
+
+    /**
+     * Creates a new MessageManager.
+     */
+    public MessageManager() {
+        this.messages = new HashMap<UUID, Message>();
+    }
 }
