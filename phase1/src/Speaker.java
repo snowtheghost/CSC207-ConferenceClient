d1971b4a3cc72cfe59b1fdd3acf826dbdcf5a120
Derrick Wang <derrick.wang@mail.utoronto.ca>
2020-11-04 19:54:42 -0800

    add some methods in Speaker.java

--- phase1/src/Speaker.java
+++ phase1/src/Speaker.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Represent a Speaker
+ * @author Zihan Wang
+ */
 public class Speaker extends User {
+    private List<UUID> eventsSpeaking;
+
     public Speaker(String username) {
         super(username);
+        eventsSpeaking = new ArrayList<>();
+    }
+
+    public List<UUID> getEventsSpeaking() {
+        return eventsSpeaking;
     }
 }
