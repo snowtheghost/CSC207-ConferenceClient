7240af413b05c60631a3b85dfeefe98e00ab529a
Tanuj Devjani <tanuj.devjani@mail.utoronto.ca>
2020-11-06 23:30:33 -0500

    added methods/comments clarifying code

--- phase1/src/Organizer.java
+++ phase1/src/Organizer.java
@@ -1,5 +1,13 @@
-public class Organizer extends Attendee {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+/**
+ * Represents an Organizer, the most powerful form of User
+ * Author: Tanuj Devjani
+ */
+public class Organizer extends Attendee {
+    private List<UUID> eventsOrganized;
     /**
      * Creates a new user with a unique ID and a username.
      *
@@ -7,10 +15,19 @@ public class Organizer extends Attendee {
      */
     public Organizer(String username) {
         super(username);
+        eventsOrganized = new ArrayList<>();
     }
 
     @Override
+    /*
+     method used as an attribute to check whether or not a given user is an organizer or not
+     */
     public boolean isOrganizer() {
         return true;
     }
+    public List<UUID> getEvents() {
+        return eventsOrganized;
+    }
+
+
 }
