32195aef764cfa95708b1041b924a3ef1167371c
ZhuKe <jiayu.zhu@mail.utoronto.ca>
2020-11-07 17:14:33 -0800

    Login system now handles user inputs, waiting on UserManager methods to be completed.

--- phase1/src/LoginSystem.java
+++ phase1/src/LoginSystem.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * The login system.
  */
@@ -7,6 +9,30 @@ public class LoginSystem {
     private OrganizerPanel orgPanel;
     private SpeakerPanel spPanel;
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Enter username and press Enter
+        System.out.println("\"Login\" or \"Create account\"?");
+        String decision = input.nextLine();
+
+        if (decision.equals("login")) {
+            System.out.println("To create account, enter account name");
+            String userName = input.nextLine();
+            // TO DO check if user name exists, if so pass it onto the correct Panel
+
+        }
+        else if (decision.equals("create account")) {
+            System.out.println("Enter new username name and type");
+            String userName = input.nextLine();
+            // have a while loop, asking the user for a account name and type
+            // until unique account name and type given, then create account
+            // in user manager and pass onto correct panel
+        }
+        else {
+            System.out.println("invalid input");
+        }
+    }
     /**
      * Allows users to login to their account or create one.
      * @param userMan the user manager use case class
