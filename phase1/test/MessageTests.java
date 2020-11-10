c418f69b99a143ac9a1514b1436c82f44c56b76a
Liam <ogilvieliam@gmail.com>
2020-11-09 22:51:09 -0500

    Tests added to MessageTests, MessageManagerTests created and tests added

--- phase1/test/MessageTests.java
+++ phase1/test/MessageTests.java
@@ -1,20 +1,25 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.util.UUID;
-import java.util.List;
-import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Zachariah Vincze
+ * @author Liam Ogilvie
  */
 
 public class MessageTests {
     private Message message1;
+    private Message message2;
     private String messageContent;
 
     @Before
     public void setupBefore() {
+        String messageContent = "hello";
         message1 = new Message(messageContent);
+        message2 = new Message(messageContent);
     }
 
     @Test
@@ -25,6 +30,11 @@ public class MessageTests {
 
     @Test
     public void testGetMessageContent() {
-        assertEquals(message1.getMessageContent(), messageContent);
+        assertEquals(message1.getMessageContent(), "hello");
+    }
+
+    @Test
+    public void testRandomUUIDGeneration() {
+        assertNotSame(message1.getMessageID(), message2.getMessageID());
     }
 }
