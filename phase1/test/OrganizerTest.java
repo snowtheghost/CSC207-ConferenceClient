9ac6553974b1de94e059f29667ce604fce6afe61
Derrick Wang <derrick.wang@mail.utoronto.ca>
2020-11-09 18:21:07 -0800

    Add some tests in OrganizerTest

--- phase1/test/OrganizerTest.java
+++ phase1/test/OrganizerTest.java
@@ -1,13 +1,17 @@
 import org.junit.Test;
+import java.util.UUID;
+import java.util.ArrayList;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @author Zihan Wang
  */
 public class OrganizerTest {
     Organizer organizer1 = new Organizer("organizer1");
+    Message message1 = new Message("message1");
+    Attendee attendee1 = new Attendee("attendee1");
+
 
     @Test
     public void TestIsOrganizer(){
@@ -19,4 +23,16 @@ public class OrganizerTest {
 
     @Test
     public void TestIsSpeaker(){assertFalse(organizer1.isSpeaker());}
+
+    @Test
+    public void TestGetUserName(){assertEquals(organizer1.getUsername(), "organizer1");}
+
+    @Test
+    public void TestAddGetMessage(){
+        ArrayList<UUID> res = new ArrayList<>();
+        assertEquals(res, organizer1.getMessages(attendee1.getUserID()));
+        organizer1.addMessage(attendee1.getUserID(), message1.getMessageID());
+        res.add(message1.getMessageID());
+        assertEquals(res, organizer1.getMessages(attendee1.getUserID()));
+    }
 }
