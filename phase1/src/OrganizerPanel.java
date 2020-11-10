31fc61ef60fd158c86f5c912f821c374ceb93d81
Justin Chan <67134961+snowtheghost@users.noreply.github.com>
2020-11-09 19:06:52 -0500

    OrganizerPanel sample

--- phase1/src/OrganizerPanel.java
+++ phase1/src/OrganizerPanel.java
@@ -1,15 +1,42 @@
 import java.util.*;
 
 /**
- * @author Justin Chan, Tanuj Devjani, Zachariah Vincze
+ * @author Justin Chan, Tanuj Devjani
  */
 public class OrganizerPanel implements IController {
-    private boolean quitting = false;
+    private final boolean quitting = false;
     private final Scanner sc = new Scanner(System.in);
+    private final UserManager um;
+    private final RoomManager rm;
+
+    /**
+     * @param rm the RoomManager
+     * Last modified: Justin Chan
+     */
+    OrganizerPanel(UserManager um, RoomManager rm) {
+        this.um = um;
+        this.rm = rm;
+    }
 
     public void createEvent(){
         System.out.print("Room: ");
-        String room = sc.nextLine();
+        int room = Integer.parseInt(sc.nextLine());
+        System.out.print("Event Title: ");
+        String title = sc.nextLine();
+        System.out.print("Speaker username: ");
+        String speakerName = sc.nextLine();
+        System.out.print("Year: ");
+        int year = Integer.parseInt(sc.nextLine());
+        System.out.print("Month: ");
+        int month = Integer.parseInt(sc.nextLine()) - 1;
+        System.out.print("Day: ");
+        int day = Integer.parseInt(sc.nextLine());
+        System.out.print("Start hour: ");
+        int hour = Integer.parseInt(sc.nextLine());
+
+        // Assume checked
+        Speaker speaker = (Speaker) um.getUsernameToUser().get(speakerName);
+        rm.newEvent(title, speaker, new GregorianCalendar(year, month, day, hour, 0, 0), new GregorianCalendar(year, month, day, hour + 1, 0, 0), rm.getRooms().get(0));
     }
 
     public boolean deleteEvent(){
