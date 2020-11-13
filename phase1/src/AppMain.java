fd2b0c66357cc03fc06ce911addbf95a9e227da8
Zachary Vincze <zacharyvincze@gmail.com>
2020-11-13 17:02:51 -0500

    Name changes and some refactoring for better readability.

--- phase1/src/AppMain.java
+++ phase1/src/AppMain.java
@@ -11,49 +11,62 @@ public class AppMain {
     public static void main(String[] args) {
         boolean applicationRunning = true;
 
+        // Initialize all required components of the application.
         // Gateways
         MessageManagerGateway messageManagerGateway= new MessageManagerGateway();
         UserManagerGateway userManagerGateway = new UserManagerGateway();
         RoomManagerGateway roomManagerGateway = new RoomManagerGateway();
 
+        // Use Cases
         UserManager userManager = userManagerGateway.read("usermanager.ser");
         RoomManager roomManager = roomManagerGateway.read("roommanager.ser");
         MessageManager messageManager = messageManagerGateway.read("messagemanager.ser");
+
         InputFilter inputFilter = new InputFilter(userManager, roomManager);
 
+        // Controllers
         LoginSystem loginSystem = new LoginSystem(userManager);
         AttendeePanel attendeePanel = new AttendeePanel(userManager, messageManager, roomManager, inputFilter);
         OrganizerPanel organizerPanel = new OrganizerPanel(userManager, roomManager);
         SpeakerPanel speakerPanel = new SpeakerPanel(userManager, messageManager, roomManager);
 
-        // Begin constructing the menu
+        // Construct a MenuTree using the initialized controllers
         MenuTree attendeeMenu = new MenuTree(attendeePanel);
         MenuTree organizerMenu = new MenuTree(organizerPanel);
         MenuTree speakerMenu = new MenuTree(speakerPanel);
 
-        MenuTree loginMenu = new MenuTree(loginSystem);
-        loginMenu.addChild(attendeeMenu);
-        loginMenu.addChild(organizerMenu);
-        loginMenu.addChild(speakerMenu);
+        MenuTree mainMenu = new MenuTree(loginSystem);
+        mainMenu.addChild(attendeeMenu);
+        mainMenu.addChild(organizerMenu);
+        mainMenu.addChild(speakerMenu);
 
 
         while (applicationRunning) {
-            int command = loginMenu.run();
+            int command = mainMenu.run();
             switch (command) {
                 case -3:
+                    // Quits the application.
                     applicationRunning = false;
                     break;
                 case -2:
-                    loginMenu = loginMenu.getParent();
+                    // Ensure that there exists parent node.
+                    if (mainMenu.getParent() != null) {
+                        mainMenu = mainMenu.getParent();
+                    }
                     break;
                 case -1:
+                    // Does nothing (remains in the same state)
                     break;
                 default:
-                    loginMenu = loginMenu.getChild(command);
+                    // Ensure that the requested state exists.
+                    if (mainMenu.getChild(command) != null) {
+                        mainMenu = mainMenu.getChild(command);
+                    }
                     break;
             }
         }
 
+        // Attempt to write Use Case data to external files.
         try {
             userManagerGateway.write(userManager, "usermanager.ser");
             roomManagerGateway.write(roomManager, "roommanager.ser");
--- phase1/src/MenuTree.java
+++ phase1/src/MenuTree.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 /**
  * Represents a MenuTree
- * @author Kaiyi Liu, Zachary Vince
+ * @author Kaiyi Liu, Zachariah Vince
  * */
 
 public class MenuTree {
@@ -11,17 +11,32 @@ public class MenuTree {
     private IController value;
     private List<MenuTree> childMenuTrees; //if null it is youngest
 
+    /**
+     * Creates a parent MenuTree (with no parents)
+     * @param value the controller this menu option corresponds to.
+     */
     public MenuTree(IController value){
         this.value = value;
         this.parentMenuTree = null;
         this.childMenuTrees = new ArrayList<>();
     }
 
+    /**
+     * Creates a Menu tree with a parent.
+     * @param value the controller this menu option corresponds to.
+     * @param parent the parent of this menu option.
+     */
     public MenuTree(IController value, MenuTree parent){
         this(value);
         this.parentMenuTree = parent;
     }
 
+    /**
+     * Creates a MenuTree with a parent and a list of children.
+     * @param value the controller this menu option corresponds to.
+     * @param parent the parent of this menu option.
+     * @param children the children menus of this menu option.
+     */
     public MenuTree(IController value, MenuTree parent, List<MenuTree> children){
         this(value, parent);
         this.childMenuTrees = children;
@@ -43,19 +58,34 @@ public class MenuTree {
         return this.parentMenuTree;
     }
 
+    /**
+     * @param index the index number of the children node list.
+     * @return the child MenuTree at this index.
+     */
     public MenuTree getChild(int index){
         return this.childMenuTrees.get(index);
     }
 
+    /**
+     * @return a list of this node's children.
+     */
     public List<MenuTree> getChildren(){
         return new ArrayList<>(this.childMenuTrees);
     }
 
+    /**
+     * Adds a child node to this tree.
+     * @param tree the MenuTree you wish to add.
+     */
     public void addChild(MenuTree tree){
         tree.setParent(this);
         this.childMenuTrees.add(tree);
     }
 
+    /**
+     * Adds a list of children nodes to this tree.
+     * @param trees the list of MenuTrees to add.
+     */
     public void addChildren(List<MenuTree> trees){
         for(MenuTree tree : trees){
             tree.setParent(this);
@@ -63,13 +93,24 @@ public class MenuTree {
         }
     }
 
+    /**
+     * Changes the controller class this menu option corresponds to.
+     * @param controller the controller class to change to.
+     */
     public void setValue(IController controller){
         this.value = controller;
     }
 
+    /**
+     * @return the controller class this menu option corresponds to.
+     */
     public IController getValue(){
         return this.value;
     }
 
+    /**
+     * Call's the controller's run command.
+     * @return the state change command int.
+     */
     public int run() { return this.value.run(); }
 }
