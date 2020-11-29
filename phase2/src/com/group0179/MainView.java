75349666d3c8b841df3fcf8983576bbb91e87864
Zachary Vincze <zacharyvincze@gmail.com>
2020-11-28 21:25:08 -0500

    Began work on LoginScene layout

--- phase2/src/com/group0179/MainView.java
+++ phase2/src/com/group0179/MainView.java
@@ -52,7 +52,8 @@ public class MainView extends Application {
     @Override
     public void start(Stage primaryStage) {
         stage = primaryStage;
-        setOrganizerScene(); // Should start on loginScene
+        // setOrganizerScene(); // Should start on loginScene
+        setLoginScene();
         stage.show();
     }
 }
--- phase2/src/com/group0179/scenes/LoginScene.java
+++ phase2/src/com/group0179/scenes/LoginScene.java
@@ -15,17 +15,75 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 /**
- * @author Justin Chan
+ * LoginScene implementation. Responsible
+ * for handling user login and account creation.
+ * @author Zachariah Vincze
  */
 
 public class LoginScene implements IScene {
     LoginFilter filter;
     LoginPresenter presenter;
-    BorderPane main = new BorderPane();
-    HBox topMenu = new HBox();
-    HBox emptyBottomMenu = new HBox();
+    BorderPane borderPane;
+    Scene mainScene;
 
-    Scene mainPanel;
+    // Buttons
+    Button loginScreenBtn;
+    Button createAccountScreenBtn;
+
+    Button loginBtn;
+    Button createAccountBtn;
+
+    // TextFields
+    TextField loginTextField;
+    TextField createAccountTextField;
+
+    // Grids
+    GridPane loginGrid;
+    GridPane createAccountGrid;
+
+    // Top menu
+    HBox topMenu;
+
+    private void changeToLoginPane() {
+        borderPane.setCenter(loginGrid);
+    }
+
+    private void changeToCreateAccountPane() {
+        borderPane.setCenter(createAccountGrid);
+    }
+
+    private void constructScene() {
+        loginScreenBtn = new Button("Login");
+        createAccountScreenBtn = new Button("Create Account");
+        topMenu = new HBox();
+        topMenu.getChildren().addAll(loginScreenBtn, createAccountScreenBtn);
+
+        borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+
+        // Create text fields
+        loginTextField = new TextField();
+        createAccountTextField = new TextField();
+
+        // Create buttons
+        loginBtn = new Button("Login");
+        createAccountBtn = new Button("Create account");
+
+        // Construct login grid
+        loginGrid = new GridPane();
+        loginGrid.add(loginTextField, 0, 0);
+        loginGrid.add(loginBtn, 1, 0);
+
+        // Construct create account grid
+        createAccountGrid = new GridPane();
+        createAccountGrid.add(createAccountTextField, 0, 0);
+        createAccountGrid.add(createAccountBtn, 1, 0);
+
+        // Set the default main grid
+        borderPane.setCenter(loginGrid);
+
+        mainScene = new Scene(borderPane, x, y);
+    }
 
     public LoginScene(LoginFilter filter, LoginPresenter presenter) {
         this.filter = filter;
@@ -34,103 +92,7 @@ public class LoginScene implements IScene {
 
     @Override
     public void setScene() {
-        // Layout and scene for Full View
-        BorderPane main = new BorderPane();
-        mainPanel = new Scene(main, x, y);
-
-        // Layout and scene for Menu
-        HBox topMenu = new HBox();
-        topMenu.setSpacing(10);
-
-        // Layout and scene for Login
-        GridPane loginMenu = new GridPane();
-        loginMenu.setVgap(2.5);
-        loginMenu.setHgap(2.5);
-
-        // Layout and scene for Account Creation
-        GridPane createAccount = new GridPane();
-        createAccount.setVgap(2.5);
-        createAccount.setHgap(2.5);
-
-        // Elements for the Main Menu
-        Button loginMenuButton = new Button("Login");
-        loginMenuButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                main.setCenter(loginMenu);
-                MainView.getStage().setTitle("Login Menu: Login");
-            }
-        });
-
-        Button button2 = new Button("Create Account"); // TODO:
-        button2.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                main.setCenter(createAccount);
-                MainView.getStage().setTitle("Login Menu: Create Account");
-            }
-        });
-
-
-        // Elements for Login Menu
-        Label inputNamePrompt = new Label("Username: ");
-        TextField inputName = new TextField();
-
-        Label loginFailedLabel = new Label("User was not found.");
-
-        Button loginButton = new Button("Login");
-        loginButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                loginMenu.getChildren().remove(loginFailedLabel);
-                if (!false) { // TODO
-                    loginMenu.add(loginFailedLabel, 5, 7, 2, 1);
-                }
-            }
-        });
-
-
-        // Elements for Account Creation
-        Label inputNewNamePrompt = new Label("Username: ");
-        TextField inputNewName = new TextField();
-
-        Label creationSuccessLabel = new Label("User created successfully..");
-        Label creationFailedLabel = new Label("Username already exists.");
-
-        Button createAccountButton = new Button("Create");
-        loginButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                createAccount.getChildren().remove(creationSuccessLabel);
-                createAccount.getChildren().remove(creationFailedLabel);
-                if (!false) { // TODO
-                    createAccount.add(creationSuccessLabel, 5, 7, 2, 1);
-                } else {
-                    createAccount.add(creationFailedLabel, 5, 7, 2, 1);
-                }
-            }
-        });
-
-
-        // Set initial scene for Main Panel
-        main.setTop(topMenu);
-
-        // Add buttons to Top Menu
-        topMenu.getChildren().addAll(loginMenuButton, button2);
-
-        // Add buttons to Login Menu
-        loginMenu.add(loginButton, 7, 5);
-        loginMenu.add(inputNamePrompt, 5, 5);
-        loginMenu.add(inputName, 6, 5);
-
-        // Add buttons to Create Account
-        createAccount.add(createAccountButton, 7, 5);
-        createAccount.add(inputNewNamePrompt, 5, 5);
-        createAccount.add(inputNewName, 6, 5);
-
-
-        // Setup and Start Initial Scene: Main Menu
-        MainView.getStage().setScene(mainPanel);
-        MainView.getStage().setTitle("Login Panel");
+        constructScene();
+        MainView.getStage().setScene(mainScene);
     }
 }
