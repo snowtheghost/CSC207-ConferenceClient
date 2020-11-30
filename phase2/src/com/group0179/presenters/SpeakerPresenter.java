08299447d0b1f378789ab8b6c4ac65444af081a7
Liam <ogilvieliam@gmail.com>
2020-11-30 14:35:50 -0500

    Worked on message support, added messaging functionality to SpeakerScene

--- phase2/src/com/group0179/presenters/SpeakerPresenter.java
+++ phase2/src/com/group0179/presenters/SpeakerPresenter.java
@@ -4,8 +4,7 @@ import com.group0179.use_cases.MessageManager;
 import com.group0179.use_cases.RoomManager;
 import com.group0179.use_cases.UserManager;
 
-import java.util.ArrayList;
-import java.util.UUID;
+import java.util.*;
 
 public class SpeakerPresenter extends Presenter {
     private final UserManager userMan;
@@ -19,22 +18,92 @@ public class SpeakerPresenter extends Presenter {
     }
 
     public ArrayList<String> getEventListArray() {
-        ArrayList<UUID> eventIDs = roomMan.getEventIDs();
+        ArrayList<UUID> eventIDs = this.roomMan.getEventIDs();
         ArrayList<String> events = new ArrayList<>();
         for (UUID eventID: eventIDs) {
-            events.add(roomMan.stringEvent(eventID));
+            events.add(this.roomMan.stringEvent(eventID));
         } return events;
     }
 
     public ArrayList<String> getSpeakingEventListArray() {
-        ArrayList<UUID> eventIDs = userMan.getSpeakerEventIDs(userMan.getCurrentUser());
+        ArrayList<UUID> eventIDs = this.userMan.getSpeakerEventIDs(this.userMan.getCurrentUser());
         ArrayList<String> events = new ArrayList<>();
         for (UUID eventID: eventIDs) {
-            events.add(roomMan.stringEvent(eventID));
+            events.add(this.roomMan.stringEvent(eventID));
         } return events;
     }
 
+    public ArrayList<UUID> getAttendees() {
+        return this.userMan.getAttendeeUUIDs();
+    }
+
+    public String getUsername(UUID id){return this.userMan.getUsername(id);}
+
+    public Iterator<Map.Entry<UUID, List<String>>> viewMessages(){
+        HashMap<UUID, List<String>> fmessages = new HashMap<>();
+        for (UUID id : this.userMan.getAttendeeUUIDs()){
+            List<String> messages = this.msgMan.getMessageContentsFromUser(this.userMan, this.userMan.getCurrentUser(), id);
+            if (messages.size() > 0){
+                fmessages.put(id, messages);
+            }
+
+        }
+        Iterator<Map.Entry<UUID, List<String>>> entrySet = fmessages.entrySet().iterator();
+        return entrySet;
+    }
+
+    public String message(String username, String content){
+        if (!userExists(username)) {
+            return "Recipient does not exist";
+        }
+        UUID recipient = this.userMan.getUserID(username);
+        this.msgMan.sendMessage(this.userMan, this.userMan.getCurrentUser(), recipient, content);
+        return "Message sent successfully.";
+    }
+
+    public String viewMessagess(String input){
+        ArrayList<UUID> allUserIds = new ArrayList<>(this.userMan.getAttendeeUUIDs());
+        // if want all messages, get a list of messages from each user and
+        // write the sender name and message contents if user received at least 1 message from them
+        if (input.equals("all")){
+            StringBuilder allMsgs = new StringBuilder("Messages:\n");
+            for (UUID uuid : allUserIds){
+                List<String> msgsFromPerson = this.msgMan.getMessageContentsFromUser(this.userMan, userMan.getCurrentUser(), uuid);
+                if (msgsFromPerson.size()!=0){
+                    String senderName = this.userMan.getUsername(uuid);
+                    allMsgs.append(senderName).append(": ");
+                    for (String msg : msgsFromPerson) {
+                        allMsgs.append(msg).append(", ");
+                    }
+                    allMsgs.append("\n");
+                }
+            }
+            return allMsgs.toString();
+        }
+        // if user wants message from specific user
+        UUID recipient = this.userMan.getUserID(input);
+        if (recipient == null) {return "User not found";}
+        List<String> msgContent = this.msgMan.getMessageContentsFromUser(this.userMan, userMan.getCurrentUser(), recipient);
+        return String.join(",", msgContent);
+    }
+
+
     public String viewAllEventsButton() { return "View All Events"; }
     public String viewSpeakingEventsButton() { return "View All Speaking Events"; }
+
+    /**
+     * Takes a username and returns true iff username is from an existing user
+     * also prints a message telling user if username does not exist.
+     * @param username the given username as a string
+     * @return Whether the username is from an existing user
+     */
+    private boolean userExists(String username) {
+        if (this.userMan.getUsernames().contains(username)) {
+            return true;
+        }
+        //this.displayUserDoesNotExistError();
+        System.out.println("User not found");
+        return false;
+    }
 
 }
--- phase2/src/com/group0179/scenes/SpeakerScene.java
+++ phase2/src/com/group0179/scenes/SpeakerScene.java
@@ -7,13 +7,23 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author Template: Justin Chan
@@ -28,21 +38,45 @@ public class SpeakerScene implements IScene {
     GridPane menu2 = new GridPane();
     GridPane menu2sub1 = new GridPane();
     BorderPane main = new BorderPane();
-    HBox topMenu = new HBox();
+    //HBox topMenu = new HBox();
+    private GridPane bottomMenu;
+    private FlowPane topMenu;
     HBox emptyBottomMenu = new HBox();
 
 
+
+
     public SpeakerScene(SpeakerFilter filter, SpeakerPresenter presenter) {
+        //bottomMenu = new GridPane();
+        //topMenu = new FlowPane();
         this.filter = filter;
         this.presenter = presenter;
+
+    }
+
+    private Text txtObjCreator(String text, double objectLength){
+        Text label1 = new Text(text);
+        label1.setWrappingWidth(objectLength);
+        return label1;
+    }
+
+    private void displayForm(Text label, TextField textField, GridPane gridPane, int gridIndex){
+        GridPane.setConstraints(label, 0, gridIndex);
+        gridPane.getChildren().add(label);
+        GridPane.setConstraints(textField, 0, gridIndex+1);
+        gridPane.getChildren().add(textField);
+
     }
 
     public void constructScene() {
+        bottomMenu = new GridPane();
+        topMenu = new FlowPane();
         // Layout and scene for Full View
         mainPanel = new Scene(main, x, y);
+        SpeakerScene speakerScene = this;
 
         // Layout and scene for Menu
-        topMenu.setSpacing(10);
+        //topMenu.setSpacing(10);
 
         menu2.setVgap(2.5);
         menu2.setHgap(2.5);
@@ -50,6 +84,13 @@ public class SpeakerScene implements IScene {
         menu2sub1.setHgap(2.5);
         ListView<String> viewEventList = new ListView<>();
         ListView<String> viewSpeakingEventList = new ListView<>();
+
+        ListView<String> speakerManager = new ListView<>();
+        HBox msgMenu = new HBox(); msgMenu.setSpacing(5); msgMenu.setPadding(new Insets(10, 10, 10, 10));
+        // Create Speaker
+        GridPane msgview = new GridPane(); msgview.setVgap(5); msgview.setHgap(5); msgview.setPadding(new Insets(0, 10, 0, 10));
+        GridPane msgviewBottom = new GridPane(); msgview.setVgap(5); msgview.setHgap(5); msgview.setPadding(new Insets(0, 10, 0, 10));
+
 
         // Elements for the Main Menu
         Button viewEventsButton = new Button(this.presenter.viewAllEventsButton());
@@ -72,6 +113,128 @@ public class SpeakerScene implements IScene {
             viewSpeakingEventList.setItems(eventsList);
         });
 
+        Button viewMessagesButton = new Button("View Messages");
+        viewMessagesButton.setOnAction(actionEvent -> {
+            main.setCenter(msgview);
+            main.setBottom(msgviewBottom);
+            MainView.getStage().setTitle("View Messages");
+        });
+
+        Button button1 = new Button("Send message");
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                bottomMenu.getChildren().clear();
+
+                // Creates labels and input space for form
+                Text label1 = new Text("Enter username of who you would like to message.");
+                Text label2 = new Text("Enter message content");
+                TextField textField1 = new TextField();
+                TextField textField2 = new TextField();
+
+                // adds those to pane so it can be displayed
+                displayForm(label1, textField1, bottomMenu, 0);
+                displayForm(label2, textField2, bottomMenu, 2);
+
+                // adds send button and result message to pane
+                Button submitButton = new Button("Send");
+                Text result = new Text("");
+                submitButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        result.setText(presenter.message(textField1.getText(), textField2.getText()));
+                    }
+                });
+                GridPane.setConstraints(submitButton, 0, 5);
+                bottomMenu.getChildren().add(submitButton);
+                GridPane.setConstraints(result, 0, 6);
+                bottomMenu.getChildren().add(result);
+            }
+        });
+
+        Button button2 = new Button("View messages from username");
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                bottomMenu.getChildren().clear();
+
+
+                // adds input box
+                //Text label = speakerScene.txtObjCreater("Enter username of who's messages you would like to see, enter 'all' for all.", x/1.5);
+                Text label = new Text("Enter Username:");
+                TextField textField = new TextField();
+                displayForm(label, textField, bottomMenu, 0);
+
+                // adds submit button and result
+                Text result = new Text("[autocomplete can go here]");
+                Button submitButton = new Button("Submit");
+                submitButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        // adds result when button pressed
+                        result.setText(presenter.viewMessagess(textField.getText()));
+                    }
+                });
+                GridPane.setConstraints(submitButton, 0, 3);
+                bottomMenu.getChildren().add(submitButton);
+                GridPane.setConstraints(result, 0, 4);
+                bottomMenu.getChildren().add(result);
+
+
+            }
+        });
+
+
+
+        Button reply1Button = new Button("Reply");
+        reply1Button.setOnAction(actionEvent -> {
+            //main.setCenter(msgview);
+
+            MainView.getStage().setTitle("Reply to Message 1");
+        });
+
+        Button reply2Button = new Button("Reply");
+        reply2Button.setOnAction(actionEvent -> {
+            //main.setCenter(msgview);
+            MainView.getStage().setTitle("Reply to Message 2");
+        });
+
+        Button reply3Button = new Button("Reply");
+        reply3Button.setOnAction(actionEvent -> {
+            //main.setCenter(msgview);
+            MainView.getStage().setTitle("Reply to Message 3");
+        });
+
+        Button reply4Button = new Button("Reply");
+        reply4Button.setOnAction(actionEvent -> {
+            //main.setCenter(msgview);
+            MainView.getStage().setTitle("Reply to Message 4");
+        });
+
+        Button reply5Button = new Button("Reply");
+        reply5Button.setOnAction(actionEvent -> {
+            //main.setCenter(msgview);
+            MainView.getStage().setTitle("Reply to Message 5");
+        });
+
+        Button reply6Button = new Button("Reply");
+        reply6Button.setOnAction(actionEvent -> {
+            //main.setCenter(msgview);
+            MainView.getStage().setTitle("Reply to Message 6");
+        });
+
+        Button reply7Button = new Button("Reply");
+        reply7Button.setOnAction(actionEvent -> {
+            //main.setCenter(msgview);
+            MainView.getStage().setTitle("Reply to Message 7");
+        });
+
+        Button reply8Button = new Button("Reply");
+        reply8Button.setOnAction(actionEvent -> {
+            //main.setCenter(msgview);
+            MainView.getStage().setTitle("Reply to Message 8");
+        });
+
 
         Button logoutButton = new Button("Log out");
         logoutButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -85,8 +248,101 @@ public class SpeakerScene implements IScene {
         main.setTop(topMenu);
 
         // Add buttons to Top Menu
-        topMenu.getChildren().addAll(viewEventsButton, viewSpeakingEventsButton, logoutButton);
+        topMenu.getChildren().addAll(viewEventsButton, viewSpeakingEventsButton, button1, viewMessagesButton, logoutButton);
+
+        //System.out.println(presenter.viewMessagess("LiamA"));
+
+        int i = 0;
+
+        for (Iterator<Map.Entry<UUID, List<String>>> it = this.presenter.viewMessages(); it.hasNext(); ) {
+            System.out.println(this.presenter.viewMessages());
+            Map.Entry<UUID, List<String>> key = it.next();
+            //System.out.println(key);
+            //System.out.println(key.getKey());
+            //System.out.println(key.getValue());
+            if (i == 0){
+                Label sender1 = new Label(presenter.getUsername(key.getKey()));
+                Label msg1 = new Label(key.getValue().get(0));
+                msgview.add(reply1Button, 0, 0);
+                sender1.setStyle("-fx-font-weight: bold");
+                msgview.add(sender1, 1, 0);
+                msgview.add(msg1, 2, 0);
+            }
+            if (i == 1){
+                Label sender2 = new Label(presenter.getUsername(key.getKey()));
+                Label msg2 = new Label(key.getValue().get(0));
+                msgview.add(reply2Button, 0, 1);
+                sender2.setStyle("-fx-font-weight: bold");
+                msgview.add(sender2, 1, 1);
+                msgview.add(msg2, 2, 1);
+            }
+            if (i == 2){
+                Label sender3 = new Label(presenter.getUsername(key.getKey()));
+                Label msg3 = new Label(key.getValue().get(0));
+                msgview.add(reply3Button, 0, 2);
+                sender3.setStyle("-fx-font-weight: bold");
+                msgview.add(sender3, 1, 2);
+                msgview.add(msg3, 2, 2);
+            }
+            if (i == 3){
+                Label sender4 = new Label(presenter.getUsername(key.getKey()));
+                Label msg4 = new Label(key.getValue().get(0));
+                msgview.add(reply4Button, 0, 3);
+                sender4.setStyle("-fx-font-weight: bold");
+                msgview.add(sender4, 1, 3);
+                msgview.add(msg4, 2, 3);
+            }
+            if (i == 4){
+                Label sender5 = new Label(presenter.getUsername(key.getKey()));
+                Label msg5 = new Label(key.getValue().get(0));
+                msgview.add(reply5Button, 0, 4);
+                sender5.setStyle("-fx-font-weight: bold");
+                msgview.add(sender5, 1, 4);
+                msgview.add(msg5, 2, 4);
+            }
+            if (i == 5){
+                Label sender6 = new Label(presenter.getUsername(key.getKey()));
+                Label msg6 = new Label(key.getValue().get(0));
+                msgview.add(reply6Button, 0, 5);
+                sender6.setStyle("-fx-font-weight: bold");
+                msgview.add(sender6, 1, 5);
+                msgview.add(msg6, 2, 5);
+            }
+            if (i == 6){
+                Label sender7 = new Label(presenter.getUsername(key.getKey()));
+                Label msg7 = new Label(key.getValue().get(0));
+                msgview.add(reply7Button, 0, 6);
+                sender7.setStyle("-fx-font-weight: bold");
+                msgview.add(sender7, 1, 6);
+                msgview.add(msg7, 2, 6);
+            }
+            if (i == 7){
+                Label sender8 = new Label(presenter.getUsername(key.getKey()));
+                Label msg8 = new Label(key.getValue().get(0));
+                msgview.add(reply8Button, 0, 7);
+                sender8.setStyle("-fx-font-weight: bold");
+                msgview.add(sender8, 1, 7);
+                msgview.add(msg8, 2, 7);
+            }
+
+
+
+            i++;
+        }
+
+        msgviewBottom.add(button2, 1, 9);
+
+        bottomMenu.setAlignment(Pos.TOP_LEFT);
+        //bottomMenu.getChildren().add(this.txtObj);
+        int maxWidth = (x/3)*2;
+        bottomMenu.setMaxWidth(maxWidth);
+        bottomMenu.setVgap(5);
+        bottomMenu.setHgap(5);
+
+        main.setLeft(bottomMenu);
     }
+
+
 
     public void setScene() {
         MainView.getStage().setScene(mainPanel);
