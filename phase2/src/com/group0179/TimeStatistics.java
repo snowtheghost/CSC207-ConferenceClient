96b354545add63304fbc97294a0743b07550538c
Kaiyi <kaiyiliu21@gmail.com>
2020-12-03 17:35:34 -0500

    Added JavaDoc explanation

--- phase2/src/com/group0179/TimeStatistics.java
+++ phase2/src/com/group0179/TimeStatistics.java
@@ -16,18 +16,30 @@ public class TimeStatistics {
         this.timeStamp = null;
 
     }
+    /**
+     * Commences the timer
+     */
     public void commenceTiming(){
         this.startTimeLoggedIn = Instant.now();
     }
+    /**
+     * Ends the timer
+     */
     public void concludeTiming(){
         this.timeStamp = Calendar.getInstance();
         this.finishTimeLoggedIn = Instant.now();
         long timeInMilliseconds = this.finishTimeLoggedIn.toEpochMilli() - this.startTimeLoggedIn.toEpochMilli();
         this.timeLoggedInAsMinutes = ( (double) timeInMilliseconds) / 60000;
     }
+    /**
+     * @return a double that represents total time logged in
+     */
     public double getTimeLoggedInAsMinutes(){
         return this.timeLoggedInAsMinutes;
     }
+    /**
+     * @return a Calendar object that represents the exact date user logged in
+     */
     public Calendar getTimeStamp(){
         return this.timeStamp;
     }
